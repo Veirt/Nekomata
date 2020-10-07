6e218cef1ba58242c5d7e66f53d78d40aedde916
ExLog <muhammad.doni2004@gmail.com>
2020-10-07 14:31:49 +0800

    Update

--- bot.py
+++ bot.py
@@ -4,34 +4,20 @@ from packages import *
 # Virtual Environment
 token = getenv('TOKEN')
 channel_id = int(getenv('CHANNEL'))
+prefix = getenv('PREFIX')
 
 # Prefix
-client = commands.Bot("mogu ")
+client = commands.Bot(prefix)
+
+# Load Cogs
+startup_extension = ("cogs.commands", "cogs.events")
+for ext in startup_extension:
+    client.load_extension(ext)
 
 
-""" # Load cogs
-@client.command()
-async def load(ctx, extension):
-    client.load_extension(f"cogs.{extension}")
-
-@client.command()
-async def unload(ctx, extension):
-    client.unload_extension(f"cogs.{extension}") """
-
-
-for ext in os.listdir("cogs/"):
-    if ext.endswith(".py"):
-        client.load_extension(f"cogs.{ext[:-3]}")
-
-
-@client.event
-async def on_ready():
-    await client.change_presence(activity=discord.Game("mogu mogu!"))
-    print("{} has logged in.".format(client.user))
-
-
-@tasks.loop(seconds = 5.0)
+@tasks.loop(seconds=5.0)
 async def Check_Loop():
+    global newVer
     message_channel = client.get_channel(channel_id)
     # Get version from URL
     file = urllib.request.urlopen(url[0])
@@ -44,8 +30,9 @@ async def Check_Loop():
     if newVer != latestVer:
         urllib.request.urlretrieve(url[0], filename[0])
         print("There is an update")
-        embed = discord.Embed(title = "Update Notice", description = "Mogu mogu! Patched from {} to {} ".format(latestVer[-3:], newVer[-3:]), colour = discord.Colour(0xe5d1ed))
-        embed.set_footer(text="{}-{}-{}".format(now.year,now.month,now.day))
+        embed = discord.Embed(title="Update Notice", description=f"Mogu mogu! Patched from {latestVer[-3:]} to {newVer[-3:]} ",colour=discord.Colour(0xe5d1ed))
+
+        embed.set_footer(text="{}-{}-{}".format(now.year, now.month, now.day))
         await message_channel.send(embed=embed)
         f.close()
         move('PatchInfoServer.cfg', 'latest/PatchInfoServer.cfg')
