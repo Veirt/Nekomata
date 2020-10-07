c2e2789179e10bb42472578cefe1e033ce5b6fdc
ExLog <muhammad.doni2004@gmail.com>
2020-10-07 14:30:47 +0800

    Changed to tuples

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-filename = ["PatchInfoServer.cfg"]
-url = ["http://10.6.11.11/Patch/PatchInfoServer.cfg"]
+filename = ("PatchInfoServer.cfg",)
+url = ("http://10.6.11.11/Patch/PatchInfoServer.cfg",)
