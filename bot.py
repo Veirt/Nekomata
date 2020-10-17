a1deb9746a5a7dad8268b02bd81d83de47e41e1c
ExLog <muhammad.doni2004@gmail.com>
2020-10-17 19:30:55 +0800

    Remove welcome messages

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-# Import packages
+# Import Packages
 import os
 from pathlib import Path
 
--- cogs/events.py
+++ cogs/events.py
@@ -12,18 +12,6 @@ class Events(commands.Cog):
         await self.client.change_presence(activity=discord.Game("mogu mogu!"))
         print("{} has logged in.".format(self.client.user))
 
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        await self.client.get_channel(channel_id).send(
-            f"Hey {member.mention} Welcome to **{member.guild.name}**!"
-        )
-
-    @commands.Cog.listener()
-    async def on_member_remove(self, member):
-        await self.client.get_channel(channel_id).send(
-            f"{member.name}#{member.discriminator} just left the server."
-        )
-
 
 def setup(client):
     client.add_cog(Events(client))
