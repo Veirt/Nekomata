49a3731480c8b3f7ea4fdb7113ac17a558b8182b
ExLog <55097092+ExLog@users.noreply.github.com>
2021-05-22 23:06:29 +0800

    feat: add remove command to uninitialize

--- src/commands/setup.ts
+++ src/commands/setup.ts
@@ -5,18 +5,33 @@ import { getConnection } from "typeorm";
 
 export default (client: Discord.Client, prefix: string) => {
   client.on('message', async msg => {
+    if (msg.channel.type === "dm") return;
+
     if (msg.content === prefix + 'init') {
       try {
         await getConnection().getRepository(Channel).save({ server: msg.guild!.id, channel: msg.channel.id })
-        msg.channel.send("Setup channel here")
+        msg.channel.send("Initialize")
 
       } catch (err) {
         if (err.name === "QueryFailedError")
           await getConnection().getRepository(Channel).update({ server: msg.guild!.id },
             { channel: msg.channel.id })
 
-        msg.channel.send("Success update channel")
+        msg.channel.send("Reinitialize")
       }
+    }
+
+    if (msg.content === prefix + 'remove') {
+      try {
+        await getConnection().getRepository(Channel).findOneOrFail({ server: msg.guild!.id })
+
+        await getConnection().getRepository(Channel).delete({ server: msg.guild!.id })
+        msg.channel.send("Uninitialized")
+
+      } catch (err) {
+        if (err.name === "EntityNotFound") msg.channel.send("Not initialized")
+      }
+
     }
 
     const message = msg.content.split(' ')
